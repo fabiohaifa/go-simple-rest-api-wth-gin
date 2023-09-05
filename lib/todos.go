396451fd92c94084d1f7caabc02ff6a8eee0630e
Fabio Padua <fabiopadua@microsoft.com>
2023-09-05 16:58:55 -0300

    chore: list update

--- lib/todos.go
+++ lib/todos.go
@@ -15,7 +15,7 @@ import (
 var ToDos = []mod.ToDo{
 	{ID: 1, Text: "Learn Go", Done: false},
 	{ID: 2, Text: "Learn Gin", Done: false},
-	{ID: 3, Text: "Learn Docker", Done: true},
+	{ID: 3, Text: "Learn Docker", Done: false},
 	{ID: 4, Text: "Learn ACI", Done: false},
 	{ID: 5, Text: "Learn ACA", Done: false},
 	{ID: 9997, Text: "Learn AKS", Done: false},
